7b307001ec6a473734dc6c817e2877c324d0d73d
AliR7 <alirdemon79@gmail.com>
2024-09-24 20:42:58 +0330

    create basic folder project

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      "Lalezar" : "Lalezar",
+      "Yekan" : "Yekan"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
+// @layer base {
+//   @font-face {
+//       font-family: "Dana";
+//       /* 400 */
+//       src: local("Dana"), url("../fonts/Dana/woff2/DanaFaNum-Regular.woff2") format("woff2");
+//       font-display: swap;
+//   }
