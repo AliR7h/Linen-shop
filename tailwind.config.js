485a73a4ddbb912838e77a939ab9de76ade4aeaf
AliR7 <alirdemon79@gmail.com>
2024-09-26 14:36:29 +0330

    add projects to site

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,10 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        "Lalezar" : "Lalezar",
-        "Yekan" : "Yekan"
+        "Lalezar": "Lalezar",
+        "Yekan": "Yekan"
       },
     },
   },
   plugins: [],
 }
-
-// @layer base {
-//   @font-face {
-//       font-family: "Dana";
-//       /* 400 */
-//       src: local("Dana"), url("../fonts/Dana/woff2/DanaFaNum-Regular.woff2") format("woff2");
-//       font-display: swap;
-//   }
